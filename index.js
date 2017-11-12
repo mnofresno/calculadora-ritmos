98a703dcf23ec5cf65b750f78369a91d983ab6dd
Mariano Martín Fresno <mnofresno@gmail.com>
2017-11-12 03:04:14 -0300

    Agrego capacidad de calcular permutaciones

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ var askTheUser = function()
 		output: process.stdout
 	});
 	
-	rl.question('Ingrese el numero:', (answer) => 
+	
+	var permutationKey = 'p!';
+	
+	rl.question('Ingrese el numero (Agregar ' + permutationKey + ' al final para incluir las permutaciones):', (answer) => 
 	{
-		console.log(getCombinationsThatSum(answer));	
-		process.exit(1);
+		if(answer.endsWith(permutationKey))
+		{
+			answer = answer.substr(0, answer.length - permutationKey.length);
+			console.log(getAllIncludingPermutedCombinationsThatSum(answer));
+		}
+		else
+		{
+			console.log(getCombinationsThatSum(answer));
+		}
+		process.exit(0);
 	});
 };
 
@@ -19,8 +30,52 @@ if(typeof window === 'undefined')
 	askTheUser();
 }
 
+var getAllIncludingPermutedCombinationsThatSum = function(number)
+{
+	var result = [];
+	var nonPermuted = getCombinationsThatSum(number);
+	
+	_.forEach(nonPermuted, function(np)
+	{
+		heapsPermute(np, function(p)
+		{
+			addOnce(result, p);
+		});
+	});
+	return result;
+};
+
+var addOnce = function(toWhere, addWhat)
+{
+	var exists = _.find(toWhere, function(t)
+	{
+		return t.toString() === addWhat.toString();
+	});
+	if(!exists)
+	{
+		toWhere.push(addWhat.slice());
+	}
+};
+
+// Por el momento esta funcion no se usa
+var permute = function(input)
+{
+	var output = [];
+	
+	heapsPermute(input, function(p)
+	{
+		output.push(p.slice());
+	});
+	
+	return output;
+};
+ 
 var getCombinationsThatSum = function(number)
 {
+	if(isNaN(number))
+	{
+		return "El valor ingresado no es un numero.";
+	}
 	return  getCombinationSum(getAllNumberUpTo(number), number);
 };
 
@@ -44,6 +99,7 @@ var getCombinationSum = function(candidates, target)
 	candidates = _.orderBy(candidates);
 	
 	var result = [];
+	
 	getResult(result, [], candidates, target, 0);
 	
 	return result;
@@ -63,5 +119,37 @@ var getResult = function(result, cur, candidates, target, start)
 	else if(target == 0)
 	{
 		result.push(cur.slice());
+	}
+};
+
+var swap = function (array, pos1, pos2)
+{
+	var temp = array[pos1];
+	array[pos1] = array[pos2];
+	array[pos2] = temp;
+};
+
+var heapsPermute = function (array, output, n)
+{
+	n = n || array.length; // set n default to array.length
+	if (n === 1)
+	{
+		output(array);
+	} 
+	else
+	{
+		for (var i = 1; i <= n; i += 1)
+		{
+			heapsPermute(array, output, n - 1);
+			if (n % 2)
+			{
+				var j = 1;
+			}
+			else
+			{
+				var j = i;
+			}
+			swap(array, j - 1, n - 1); // -1 to account for javascript zero-indexing
+		}
 	}
 };
