30959d904dbeeb4c330df5b9dce4260dbd0f007d
Mariano Fresno <mnofresno@gmail.com>
2018-09-08 20:05:57 -0300

    Agrego loading animation mientras se calcula

--- web/main.js
+++ web/main.js
@@ -5,22 +5,34 @@ var VM = function()
     self.incluirPermutaciones    = ko.observable(false);
     self.numeroValor             = ko.observable();
     self.permutacionesCalculadas = ko.observable('');
-    
+    self.loading                 = ko.observable(false);
+    self.cantidadResultados      = ko.observable(0);
+
     var process = {
         true: getAllIncludingPermutedCombinationsThatSum,
         false: getCombinationsThatSum
     };    
     
+    self.enableCalcular = ko.pureComputed(function()
+    {
+        return !self.loading() && parseInt(self.numeroValor()) > 0;
+    });
+
     self.calcular = function()
     {
-        var ritmos = process[self.incluirPermutaciones()](self.numeroValor());
-        var ritmosHtml = "";
-        ritmosHtml = _.map(ritmos, function(ritmo)
-        {
-            return ritmo;
-        }).join("\r\n");
-        
-        self.permutacionesCalculadas(ritmosHtml);
+        self.loading(true);
+        setTimeout(function() {
+            var ritmos = process[self.incluirPermutaciones()](self.numeroValor());
+            var ritmosHtml = "";
+            ritmosHtml = _.map(ritmos, function(ritmo)
+            {
+                return ritmo;
+            }).join("\r\n");
+            
+            self.cantidadResultados(ritmos.length);
+            self.permutacionesCalculadas(ritmosHtml);
+            self.loading(false);
+        }, 100);
     };
     
     return self;
